320c33c6956f81a69999fbb055eb295f6b42ea1a
Gore Kao <gkao@purdue.edu>
2021-05-11 20:54:00 -0400

    weights init

--- utils.py
+++ utils.py
@@ -30,3 +30,17 @@ def show_image(tensor):
     plt.figure()
     plt.imshow(tensor.numpy().transpose(1, 2, 0), cmap='gray')
     plt.show()
+
+
+def weights_init(m):
+    """
+    weights initialization for netG and netD
+    :param m: model
+    :return:
+    """
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
