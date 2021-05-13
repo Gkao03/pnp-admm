a3e642143eb15e5831a54ec8a0967eec1fb8ef33
Gore Kao <gkao@purdue.edu>
2021-05-13 16:38:29 -0400

    add type parameter

--- models.py
+++ models.py
@@ -8,17 +8,7 @@ class DnCNN(nn.Module):
     def __init__(self):
         pass
 
-    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
-        """
-        A sequential convolutional block for PokeGAN Generator v1.
-        Uses fractional-strided convolutions and ReLU activation.
-        :param in_channels: num channels in input
-        :param out_channels: num channels in output
-        :param kernel_size: n x n size of kernel
-        :param stride: stride size
-        :param padding: padding size
-        :return: convolutional block including convolution, batchnorm, relu
-        """
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding, type):
         block = nn.Sequential()
         block.add_module('conv', nn.ConvTranspose2d(in_channels=in_channels,
                                                     out_channels=out_channels,
