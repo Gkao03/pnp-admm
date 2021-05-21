7cecaca9bf72a69e0db237be8bd55237f82aa37d
Gore Kao <gkao@purdue.edu>
2021-05-20 23:59:09 -0400

    optimizer

--- denoise.py
+++ denoise.py
@@ -1,6 +1,7 @@
 from models import *
 from functions import *
 from torchsummary import summary
+import torch.optim as optim
 
 
 def init_model():
@@ -14,3 +15,4 @@ def train():
     device = get_device()
     net = init_model()
     loss_fn = net.get_lossfn()
+    optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
